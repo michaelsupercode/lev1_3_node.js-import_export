fbbb4f6e36c2bf54c501e7fe8358dc7af1e7228f
SuperCoder <supercoder@AirvonSerCoder3.fritz.box>
2022-03-25 20:39:11 +0100

    first com

---
+++ function.js
@@ -0,0 +1,16 @@
+function calc(a, b, operator) {
+
+    if (operator === "+") {
+        return a + b
+    }
+    if (operator === "-") {
+        return a - b
+    }
+    if (operator === "*") {
+        return a * b
+    }
+    if (operator === "/") {
+        return a / b
+    }
+}
+module.exports = { calc }
---
+++ index.js
@@ -0,0 +1,4 @@
+const { calc } = require("./function")
+
+const res = calc(50, 10, "*")
+console.log(res);
